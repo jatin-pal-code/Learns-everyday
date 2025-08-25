5bdab1e82d88dfa036f59205604c485fa49be77d
Jatin Pal <paljatin681@gmail.com>
2025-08-25 23:46:16 +0530

    add jwt token base auth

--- HTTP-AUTH/index.js
+++ HTTP-AUTH/index.js
@@ -1,84 +1,84 @@
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
-const jwt_secret = 
+const jwt_secret = "ilovemycat"; // secret key for JWT
 
 const users = [];
 app.use(express.json());
 
-function generateToken() {
-  let options = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-  ];
+// function generateToken() {
+//   let options = [
+//     "a",
+//     "b",
+//     "c",
+//     "d",
+//     "e",
+//     "f",
+//     "g",
+//     "h",
+//     "i",
+//     "j",
+//     "k",
+//     "l",
+//     "m",
+//     "n",
+//     "o",
+//     "p",
+//     "q",
+//     "r",
+//     "s",
+//     "t",
+//     "u",
+//     "v",
+//     "w",
+//     "x",
+//     "y",
+//     "z",
+//     "A",
+//     "B",
+//     "C",
+//     "D",
+//     "E",
+//     "F",
+//     "G",
+//     "H",
+//     "I",
+//     "J",
+//     "K",
+//     "L",
+//     "M",
+//     "N",
+//     "O",
+//     "P",
+//     "Q",
+//     "R",
+//     "S",
+//     "T",
+//     "U",
+//     "V",
+//     "W",
+//     "X",
+//     "Y",
+//     "Z",
+//     "0",
+//     "1",
+//     "2",
+//     "3",
+//     "4",
+//     "5",
+//     "6",
+//     "7",
+//     "8",
+//     "9",
+//   ];
 
-  let token = "";
-  for (let i = 0; i < 32; i++) {
-    // use a simple function here
-    token += options[Math.floor(Math.random() * options.length)];
-  }
-  return token;
-}
+//   let token = "";
+//   for (let i = 0; i < 32; i++) {
+//     // use a simple function here
+//     token += options[Math.floor(Math.random() * options.length)];
+//   }
+//   return token;
+// }
 
 app.post("/signup", (req, res) => {
   const { username, password } = req.body;
@@ -106,10 +106,16 @@ app.post("/signin", (req, res) => {
   });
 
   if (user) {
-    res.json({
-      message: "Login successful",
+    const token = jwt.sign(
+      {
+        username: username,
+      },
+      jwt_secret
+    );
+    user.token = token; // Assign token to user object
+    return res.json({
+      token: token,
     });
-    user.token = generateToken(); // Assign token to user object
   } else {
     res.status(401).json({
       message: "Invalid credentials",
@@ -120,17 +126,19 @@ app.post("/signin", (req, res) => {
 
 app.get("/me", (req, res) => {
   const token = req.headers.token;
+  const decodedInformation = jwt.verify(token, jwt_secret);
+  const unAuthDecodedInfo = jwt.decode(token);
+  const username = decodedInformation.username;
 
   let foundUser = null;
   for (let i = 0; i < users.length; i++) {
-    if (users[i].token === token) {
+    if (users[i].username === username) {
       foundUser = users[i];
     }
   }
   if (foundUser) {
     res.json({
       username: foundUser.username,
-      password: foundUser.password,
     });
   } else {
     res.status(401).json({
